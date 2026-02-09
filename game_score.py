4bdf358749afb16910d1b4f55ff0daec7e11891e
bogavivek15 <bogavivek15@gmail.com>
2026-02-09 23:33:59 +0530

    initial commit

---
+++ game_score.py
@@ -0,0 +1,8 @@
+player_name=input("Enter your name :")
+games_played=int(input("Enter no of games played :"))
+score=int(input("Enter your total score :"))
+average=score/games_played
+print(f"Player: {player_name}")
+print(f"Games Played: {games_played}")
+print(f"Total Score: {score}")
+print(f"Average Score: {average}")
